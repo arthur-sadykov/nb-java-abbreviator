8fc5e2d5a752d2b9019bdbb72559f7fafbcc246d
Arthur Sadykov <arthur-sadykov@mail.ru>
2021-01-08 18:29:34 +0500

    Correct type handling within a method

--- src/com/github/isarthur/netbeans/editor/typingaid/context/impl/MethodCodeCompletionContext.java
+++ src/com/github/isarthur/netbeans/editor/typingaid/context/impl/MethodCodeCompletionContext.java
@@ -15,6 +15,7 @@
  */
 package com.github.isarthur.netbeans.editor.typingaid.context.impl;
 
+import com.github.isarthur.netbeans.editor.typingaid.abbreviation.api.Abbreviation;
 import com.github.isarthur.netbeans.editor.typingaid.collector.linker.impl.CodeFragmentCollectorLinkerImpl;
 import com.github.isarthur.netbeans.editor.typingaid.context.api.AbstractCodeCompletionContext;
 import com.github.isarthur.netbeans.editor.typingaid.insertvisitor.api.CodeFragmentInsertVisitor;
@@ -22,6 +23,10 @@ import com.github.isarthur.netbeans.editor.typingaid.insertvisitor.impl.MethodCo
 import com.github.isarthur.netbeans.editor.typingaid.request.api.CodeCompletionRequest;
 import static com.sun.source.tree.Tree.Kind.METHOD;
 import javax.lang.model.type.TypeMirror;
+import org.netbeans.api.java.lexer.JavaTokenId;
+import org.netbeans.api.java.source.TreeUtilities;
+import org.netbeans.api.java.source.WorkingCopy;
+import org.netbeans.api.lexer.TokenSequence;
 
 /**
  *
@@ -31,13 +36,63 @@ public class MethodCodeCompletionContext extends AbstractCodeCompletionContext {
 
     @Override
     protected CodeFragmentCollectorLinkerImpl getCodeFragmentCollectorLinker(CodeCompletionRequest request) {
-        return CodeFragmentCollectorLinkerImpl.builder()
-                .linkExternalTypeCollector()
-                .linkGlobalTypeCollector()
-                .linkInternalTypeCollector()
-                .linkPrimitiveTypeCollector()
-                .linkModifierCollector(METHOD)
-                .build();
+        WorkingCopy copy = request.getWorkingCopy();
+        TreeUtilities treeUtilities = copy.getTreeUtilities();
+        TokenSequence<JavaTokenId> tokenSequence = treeUtilities.tokensFor(request.getCurrentTree());
+        tokenSequence.moveStart();
+        Abbreviation abbreviation = request.getAbbreviation();
+        boolean beforeLeftParenthesis = false;
+        boolean beforeRightParenthesis = false;
+        boolean afterLeftBrace = false;
+        OUTER:
+        while (tokenSequence.moveNext()) {
+            switch (tokenSequence.token().id()) {
+                case LPAREN:
+                    if (abbreviation.getStartOffset() < tokenSequence.offset()) {
+                        beforeLeftParenthesis = true;
+                        break OUTER;
+                    }
+                    break;
+                case LBRACE:
+                    if (tokenSequence.offset() < abbreviation.getStartOffset()) {
+                        afterLeftBrace = true;
+                        break OUTER;
+                    } else {
+                        while (tokenSequence.movePrevious()) {
+                            if (tokenSequence.token().id() == JavaTokenId.RPAREN) {
+                                if (abbreviation.getStartOffset() <= tokenSequence.offset()) {
+                                    beforeRightParenthesis = true;
+                                }
+                                break OUTER;
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+        if (beforeLeftParenthesis) {
+            return CodeFragmentCollectorLinkerImpl.builder()
+                    .linkModifierCollector(METHOD)
+                    .build();
+        } else if (beforeRightParenthesis) {
+            return CodeFragmentCollectorLinkerImpl.builder()
+                    .linkExternalTypeCollector()
+                    .linkGlobalTypeCollector()
+                    .linkInternalTypeCollector()
+                    .linkPrimitiveTypeCollector()
+                    .build();
+        } else if (afterLeftBrace) {
+            return CodeFragmentCollectorLinkerImpl.builder()
+                    .linkExternalTypeCollector()
+                    .linkGlobalTypeCollector()
+                    .linkInternalTypeCollector()
+                    .linkModifierCollector(METHOD)
+                    .linkPrimitiveTypeCollector()
+                    .build();
+        } else {
+            return CodeFragmentCollectorLinkerImpl.builder()
+                    .build();
+        }
     }
 
     @Override
